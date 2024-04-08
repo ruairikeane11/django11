90996a62bb8ae8feeb0a74071c5ce579b17d15d7
ruairi keane <ruairi.rk@icloud.com>
2024-04-08 10:06:12 +0000

    Change back to false

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7t34r4czyt^1kq=o0(zwjy14izh1g0=o20^ux(rn7gf03!t_=1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '8000-ruairikeane11-django11-9iahj7umzog.ws-eu110.gitpod.io',
 '.herokuapp.com']
